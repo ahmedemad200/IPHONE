93dfc335e28baa291cc47ebde9ee41686610b3ea
ahmedemad200 <128887783+ahmedemad200@users.noreply.github.com>
2023-07-26 16:26:10 +0200

    Add files via upload

---
+++ iphone.js
@@ -0,0 +1,11 @@
+ let imc = document.querySelector('.imc');
+  let container = document.querySelector('.container');
+
+ function phones (phone)
+ {
+  imc.src = phone;
+ }
+ function colors (color)
+ {
+  container.style.background = color;
+ }
